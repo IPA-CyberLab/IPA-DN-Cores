d18cc4efc92bd14f22b348619dccdadd7418f2b7
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2019-07-14 17:39:55 +0900

    追加

--- Cores.NET/Cores.Lib/Cores.Lib.DotNet2.1.Full/InternalsVisibleTo/InternalsVisibleTo.cs
+++ Cores.NET/Cores.Lib/Cores.Lib.DotNet2.1.Full/InternalsVisibleTo/InternalsVisibleTo.cs
@@ -3,3 +3,6 @@
 [assembly: InternalsVisibleTo("AspNetCore1")]
 [assembly: InternalsVisibleTo("AspNetCore1.Views")]
 
+[assembly: InternalsVisibleTo("DN_FastReader")]
+[assembly: InternalsVisibleTo("DN_FastReader.Views")]
+
