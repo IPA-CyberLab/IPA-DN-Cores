21b0c5f0cf3fa9ce493ebfbee1024afdc93b176b
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2019-07-16 17:53:03 +0900

    InternalsVisibleTo 除去

--- Cores.NET/Cores.Lib/Cores.Lib.DotNet2.1.Full/InternalsVisibleTo/InternalsVisibleTo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("AspNetCore1")]
-[assembly: InternalsVisibleTo("AspNetCore1.Views")]
-
-[assembly: InternalsVisibleTo("DN_FastReader")]
-[assembly: InternalsVisibleTo("DN_FastReader.Views")]
-
