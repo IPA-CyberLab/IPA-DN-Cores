3a8c01c307eb0f458d501f3716a29becbfb9c444
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2019-08-02 21:22:49 +0900

    DaemonCenter 開発開始

---
+++ Cores.NET/Cores.Apps/DaemonCenter/DaemonCenter/Startup.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using IPA.Cores.Basic;
+using IPA.Cores.Helper.Basic;
+using static IPA.Cores.Globals.Basic;
+
+using IPA.Cores.AspNet;
+using IPA.Cores.Helper.AspNet;
+using static IPA.Cores.Globals.AspNet;
+
+using IPA.Cores.Basic.App.DaemonCenterLib;
+
+namespace DaemonCenter
+{
+    public class Startup
+    {
+        readonly HttpServerStartupHelper StartupHelper;
+        readonly AspNetLib AspNetLib;
+
+        public Startup(IConfiguration configuration)
+        {
+            StartupHelper = new HttpServerStartupHelper(configuration);
+
+            AspNetLib = new AspNetLib(configuration);
+
+            Configuration = configuration;
+        }
+        
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            AspNetLib.ConfigureServices(StartupHelper, services);
+
+            StartupHelper.ConfigureServices(services);
+
+            //services.Configure<CookiePolicyOptions>(options =>
+            //{
+            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+            //    options.CheckConsentNeeded = context => true;
+            //    options.MinimumSameSitePolicy = SameSiteMode.None;
+            //});
+
+            services.AddMvc()
+                .AddViewOptions(opt =>
+                {
+                    opt.HtmlHelperOptions.ClientValidationEnabled = false;
+                })
+                .AddRazorOptions(opt =>
+                {
+                    AspNetLib.ConfigureRazorOptions(opt);
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton(new Server());
+        }
+        
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime, Server server)
+        {
+            // wwwroot directory of this project
+            StartupHelper.AddStaticFileProvider(Env.AppRootDir._CombinePath("wwwroot"));
+
+            AspNetLib.Configure(StartupHelper, app, env);
+
+            StartupHelper.Configure(app, env);
+            
+            if (StartupHelper.IsDevelopmentMode)
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseHttpExceptionLogger();
+
+            app.UseStaticFiles();
+            //app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                AspNetLib._DisposeSafe();
+                StartupHelper._DisposeSafe();
+            });
+        }
+    }
+}
---
+++ Cores.NET/Cores.Basic/App/DaemonCenter/2_Server.cs
@@ -0,0 +1,79 @@
+﻿// IPA Cores.NET
+// 
+// Copyright (c) 2019- IPA CyberLab.
+// Copyright (c) 2003-2018 Daiyuu Nobori.
+// Copyright (c) 2013-2018 SoftEther VPN Project, University of Tsukuba, Japan.
+// All Rights Reserved.
+// 
+// License: The Apache License, Version 2.0
+// https://www.apache.org/licenses/LICENSE-2.0
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// 
+// THIS SOFTWARE IS DEVELOPED IN JAPAN, AND DISTRIBUTED FROM JAPAN, UNDER
+// JAPANESE LAWS. YOU MUST AGREE IN ADVANCE TO USE, COPY, MODIFY, MERGE, PUBLISH,
+// DISTRIBUTE, SUBLICENSE, AND/OR SELL COPIES OF THIS SOFTWARE, THAT ANY
+// JURIDICAL DISPUTES WHICH ARE CONCERNED TO THIS SOFTWARE OR ITS CONTENTS,
+// AGAINST US (IPA CYBERLAB, DAIYUU NOBORI, SOFTETHER VPN PROJECT OR OTHER
+// SUPPLIERS), OR ANY JURIDICAL DISPUTES AGAINST US WHICH ARE CAUSED BY ANY KIND
+// OF USING, COPYING, MODIFYING, MERGING, PUBLISHING, DISTRIBUTING, SUBLICENSING,
+// AND/OR SELLING COPIES OF THIS SOFTWARE SHALL BE REGARDED AS BE CONSTRUED AND
+// CONTROLLED BY JAPANESE LAWS, AND YOU MUST FURTHER CONSENT TO EXCLUSIVE
+// JURISDICTION AND VENUE IN THE COURTS SITTING IN TOKYO, JAPAN. YOU MUST WAIVE
+// ALL DEFENSES OF LACK OF PERSONAL JURISDICTION AND FORUM NON CONVENIENS.
+// PROCESS MAY BE SERVED ON EITHER PARTY IN THE MANNER AUTHORIZED BY APPLICABLE
+// LAW OR COURT RULE.
+
+#if CORES_BASIC_JSON && CORES_BASIC_WEBSERVER && CORES_BASIC_DAEMON
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+
+using IPA.Cores.Basic;
+using IPA.Cores.Helper.Basic;
+using static IPA.Cores.Globals.Basic;
+
+using IPA.Cores.Basic.App.DaemonCenterLib;
+
+namespace IPA.Cores.Basic.App.DaemonCenterLib
+{
+    public class Server : AsyncService
+    {
+        public Server(CancellationToken cancel = default) : base(cancel)
+        {
+        }
+    }
+
+    public class RpcServer : JsonRpcServerApi, IRpc
+    {
+#pragma warning disable CS1998
+
+        public RpcServer(CancellationToken cancel = default) : base(cancel)
+        {
+        }
+
+        public async Task<ResponseMsg> KeepAliveAsync(RequestMsg req)
+        {
+            throw new NotImplementedException();
+        }
+
+#pragma warning restore CS1998
+    }
+}
+
+#endif
+
