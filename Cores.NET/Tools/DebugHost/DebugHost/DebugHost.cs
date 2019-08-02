3413a72fbabff3b24d7634c57bcc5467760e336f
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2019-08-02 11:19:43 +0900

    DebugHost の実装

---
+++ Cores.NET/Tools/DebugHost/DebugHost/DebugHost.cs
@@ -0,0 +1,421 @@
+﻿// IPA Cores.NET
+// 
+// Copyright (c) 2018- IPA CyberLab.
+// Copyright (c) 2003-2018 Daiyuu Nobori.
+// Copyright (c) 2013-2018 SoftEther VPN Project, University of Tsukuba, Japan.
+// All Rights Reserved.
+// 
+// License: The Apache License, Version 2.0
+// https://www.apache.org/licenses/LICENSE-2.0
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// 
+// THIS SOFTWARE IS DEVELOPED IN JAPAN, AND DISTRIBUTED FROM JAPAN, UNDER
+// JAPANESE LAWS. YOU MUST AGREE IN ADVANCE TO USE, COPY, MODIFY, MERGE, PUBLISH,
+// DISTRIBUTE, SUBLICENSE, AND/OR SELL COPIES OF THIS SOFTWARE, THAT ANY
+// JURIDICAL DISPUTES WHICH ARE CONCERNED TO THIS SOFTWARE OR ITS CONTENTS,
+// AGAINST US (IPA CYBERLAB, DAIYUU NOBORI, SOFTETHER VPN PROJECT OR OTHER
+// SUPPLIERS), OR ANY JURIDICAL DISPUTES AGAINST US WHICH ARE CAUSED BY ANY KIND
+// OF USING, COPYING, MODIFYING, MERGING, PUBLISHING, DISTRIBUTING, SUBLICENSING,
+// AND/OR SELLING COPIES OF THIS SOFTWARE SHALL BE REGARDED AS BE CONSTRUED AND
+// CONTROLLED BY JAPANESE LAWS, AND YOU MUST FURTHER CONSENT TO EXCLUSIVE
+// JURISDICTION AND VENUE IN THE COURTS SITTING IN TOKYO, JAPAN. YOU MUST WAIVE
+// ALL DEFENSES OF LACK OF PERSONAL JURISDICTION AND FORUM NON CONVENIENS.
+// PROCESS MAY BE SERVED ON EITHER PARTY IN THE MANNER AUTHORIZED BY APPLICABLE
+// LAW OR COURT RULE.
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+
+namespace IPA.Cores.Tools.DebugHost
+{
+    [Flags]
+    public enum Mode
+    {
+        Unknown = 0,
+        Run,
+        Start,
+        Stop,
+        Restart,
+    }
+
+    [Flags]
+    public enum Cmd
+    {
+        Stop,
+        Start,
+        Restart,
+    }
+
+    public class Host : IDisposable
+    {
+        public string CmdLine { get; }
+
+        Thread CurrentThread = null;
+
+        int CurrentRunningProcessId = 0;
+
+        readonly AutoResetEvent StartEvent = new AutoResetEvent(false);
+
+        public Host(string cmdLine)
+        {
+            this.CmdLine = cmdLine;
+
+            Thread thread = new Thread(RunThreadProc);
+            thread.Start();
+
+            CurrentThread = thread;
+        }
+
+        void RunThreadProc()
+        {
+            LABEL_RESTART:
+            try
+            {
+                Console.WriteLine($"DebugHost: Starting the child process '{this.CmdLine}' ...");
+                Console.WriteLine("DebugHost: --------------------------------------------------------------");
+
+                using (Process p = Utils.ExecProcess(this.CmdLine))
+                {
+                    CurrentRunningProcessId = p.Id;
+
+                    try
+                    {
+                        p.WaitForExit();
+                    }
+                    finally
+                    {
+                        CurrentRunningProcessId = 0;
+                    }
+
+                    Console.WriteLine("DebugHost: --------------------------------------------------------------");
+                    Console.WriteLine($"DebugHost: Exit code of the process: {p.ExitCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DebugHost: --------------------------------------------------------------");
+                Console.WriteLine($"DebugHost: {ex.ToString()}");
+                Console.WriteLine("DebugHost: --------------------------------------------------------------");
+            }
+
+            Console.WriteLine("DebugHost: The process is stopped. Waiting for next start command ...");
+
+            StartEvent.WaitOne();
+
+            goto LABEL_RESTART;
+        }
+
+        public void Stop()
+        {
+            StartEvent.Reset();
+
+            int id = CurrentRunningProcessId;
+
+            if (id != 0)
+            {
+                try
+                {
+                    using (Process p = Process.GetProcessById(id))
+                    {
+                        p.Kill();
+                        p.WaitForExit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+            StartEvent.Reset();
+        }
+
+        public void Start()
+        {
+            StartEvent.Set();
+
+            while (true)
+            {
+                if (CurrentRunningProcessId != 0) break;
+                Thread.Sleep(1);
+            }
+        }
+
+        public void Dispose() => Dispose(true);
+        Once DisposeFlag;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing || DisposeFlag.IsFirstCall() == false) return;
+
+            if (CurrentThread != null)
+            {
+                CurrentThread.Join();
+                CurrentThread = null;
+            }
+        }
+    }
+
+    public static class Client
+    {
+        public static void ExecuteCommand(string cmdLine, string pipeName, string appId, Cmd cmd)
+        {
+            NamedPipeClientStream cli = TryConnectPipe(pipeName);
+
+            if (cli == null)
+            {
+                Console.WriteLine("DebugHost Client: The server is not running.");
+
+                if (string.IsNullOrWhiteSpace(cmdLine) == false)
+                {
+                    if (cmd == Cmd.Stop)
+                    {
+                        return;
+                    }
+
+                    Console.WriteLine($"DebugHost Client: starting the DebugHost server with cmdline '{cmdLine}' ...");
+
+                    ProcessStartInfo info = new ProcessStartInfo()
+                    {
+                        FileName = Process.GetCurrentProcess().MainModule.FileName,
+                        Arguments = $"run {appId} {cmdLine}",
+                        UseShellExecute = true,
+                        CreateNoWindow = false,
+                    };
+
+                    Process p = Process.Start(info);
+
+                    Console.WriteLine($"DebugHost Client: The debug host server process is started.");
+                    Console.WriteLine($"DebugHost Client: Waiting the server for ready ...");
+
+                    while (true)
+                    {
+                        cli = TryConnectPipe(pipeName);
+                        if (cli != null) break;
+
+                        if (p.HasExited)
+                        {
+                            throw new ApplicationException("DebugHost Client: The server process is abnormally exited.");
+                        }
+
+                        Thread.Sleep(1);
+                    }
+
+                    Console.WriteLine($"DebugHost Client: Ready.");
+                }
+                else
+                {
+                    throw new ApplicationException("DebugHost Client: The server is not running.");
+                }
+            }
+
+            using (cli)
+            {
+                Console.WriteLine($"DebugHost Client: Sending the command '{cmd.ToString()}'...");
+
+                cli.WriteByte((byte)cmd);
+
+                cli.ReadByte();
+
+                Console.WriteLine($"DebugHost Client: Command '{cmd.ToString()}' completed.");
+            }
+        }
+
+        static NamedPipeClientStream TryConnectPipe(string pipeName)
+        {
+            NamedPipeClientStream cli = null;
+            try
+            {
+                cli = new NamedPipeClientStream(pipeName);
+                cli.Connect(0);
+                return cli;
+            }
+            catch
+            {
+                if (cli != null)
+                {
+                    try
+                    {
+                        cli.Dispose();
+                    }
+                    catch { }
+                }
+                return null;
+            }
+        }
+    }
+
+    public class Server
+    {
+        public string CmdLine { get; }
+        public string PipeName { get; }
+
+        readonly Host Host;
+
+        public Server(string cmdLine, string pipeName)
+        {
+            this.CmdLine = cmdLine;
+            this.PipeName = pipeName;
+
+            this.Host = new Host(cmdLine);
+
+            while (true)
+            {
+                using (NamedPipeServerStream svr = new NamedPipeServerStream(this.PipeName))
+                {
+                    try
+                    {
+                        svr.WaitForConnection();
+
+                        Cmd cmd = (Cmd)svr.ReadByte();
+
+                        Console.WriteLine($"DebugHost: Received a command: {cmd.ToString()}");
+
+                        switch (cmd)
+                        {
+                            case Cmd.Stop:
+                                this.Host.Stop();
+                                break;
+
+                            case Cmd.Start:
+                                this.Host.Start();
+                                break;
+
+                            case Cmd.Restart:
+                                this.Host.Stop();
+                                this.Host.Start();
+                                break;
+                        }
+
+                        svr.WriteByte(1);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+            }
+        }
+    }
+
+    public static class MainClass
+    {
+        static int Main(string[] args)
+        {
+            Utils.DivideCommandLine(Environment.CommandLine, out _, out string argString);
+
+            argString = argString.Trim();
+
+            bool showHelp = false;
+
+            if (string.IsNullOrWhiteSpace(argString) == false)
+            {
+                Utils.GetKeyAndValue(argString, out string action, out string appIdAndCmdLine);
+                Utils.GetKeyAndValue(appIdAndCmdLine, out string appId, out string cmdLine);
+
+                appId = appId.ToLower();
+
+                if (string.IsNullOrWhiteSpace(argString) || string.IsNullOrWhiteSpace(appId))
+                {
+                    showHelp = true;
+                }
+                else
+                {
+                    string appIdHash = Utils.ByteToHex(Utils.HashSHA1(Encoding.UTF8.GetBytes(appId + ":HashDebugHost")), "").ToLower();
+
+                    string pipeName = "pipe_" + appIdHash;
+
+                    string mutexName = "mutex_" + appIdHash;
+
+                    Mode mode = Mode.Unknown;
+                    Cmd cmd = default;
+
+                    switch (action.ToLower())
+                    {
+                        case "run":
+                            if (string.IsNullOrWhiteSpace(cmdLine) == false)
+                            {
+                                mode = Mode.Run;
+                            }
+                            break;
+
+                        case "stop":
+                            mode = Mode.Stop;
+                            cmd = Cmd.Stop;
+                            break;
+
+                        case "start":
+                            mode = Mode.Start;
+                            cmd = Cmd.Start;
+                            break;
+
+                        case "restart":
+                            mode = Mode.Restart;
+                            cmd = Cmd.Restart;
+                            break;
+                    }
+
+                    if (mode == Mode.Unknown)
+                    {
+                        showHelp = true;
+                    }
+                    else
+                    {
+                        if (mode == Mode.Run)
+                        {
+                            GlobalMutex mutex = new GlobalMutex(mutexName);
+
+                            if (mutex.TryLock())
+                            {
+                                Server svr = new Server(cmdLine, pipeName);
+
+                                mutex.Unlock();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error: Another instance is already running.");
+
+                                return -2;
+                            }
+                        }
+                        else
+                        {
+                            Client.ExecuteCommand(cmdLine, pipeName, appId, cmd);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                showHelp = true;
+            }
+
+            if (showHelp)
+            {
+                Console.WriteLine(@"Usage:
+ DebugHost run <AppId> <CommandLine...>
+ DebugHost start <AppId> [CommandLine...]
+ DebugHost stop <AppId>
+ DebugHost restart <AppId>
+");
+
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Cores.NET/Tools/DebugHost/DebugHost/Utils.cs
@@ -0,0 +1,281 @@
+﻿// IPA Cores.NET
+// 
+// Copyright (c) 2018- IPA CyberLab.
+// Copyright (c) 2003-2018 Daiyuu Nobori.
+// Copyright (c) 2013-2018 SoftEther VPN Project, University of Tsukuba, Japan.
+// All Rights Reserved.
+// 
+// License: The Apache License, Version 2.0
+// https://www.apache.org/licenses/LICENSE-2.0
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// 
+// THIS SOFTWARE IS DEVELOPED IN JAPAN, AND DISTRIBUTED FROM JAPAN, UNDER
+// JAPANESE LAWS. YOU MUST AGREE IN ADVANCE TO USE, COPY, MODIFY, MERGE, PUBLISH,
+// DISTRIBUTE, SUBLICENSE, AND/OR SELL COPIES OF THIS SOFTWARE, THAT ANY
+// JURIDICAL DISPUTES WHICH ARE CONCERNED TO THIS SOFTWARE OR ITS CONTENTS,
+// AGAINST US (IPA CYBERLAB, DAIYUU NOBORI, SOFTETHER VPN PROJECT OR OTHER
+// SUPPLIERS), OR ANY JURIDICAL DISPUTES AGAINST US WHICH ARE CAUSED BY ANY KIND
+// OF USING, COPYING, MODIFYING, MERGING, PUBLISHING, DISTRIBUTING, SUBLICENSING,
+// AND/OR SELLING COPIES OF THIS SOFTWARE SHALL BE REGARDED AS BE CONSTRUED AND
+// CONTROLLED BY JAPANESE LAWS, AND YOU MUST FURTHER CONSENT TO EXCLUSIVE
+// JURISDICTION AND VENUE IN THE COURTS SITTING IN TOKYO, JAPAN. YOU MUST WAIVE
+// ALL DEFENSES OF LACK OF PERSONAL JURISDICTION AND FORUM NON CONVENIENS.
+// PROCESS MAY BE SERVED ON EITHER PARTY IN THE MANNER AUTHORIZED BY APPLICABLE
+// LAW OR COURT RULE.
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO.Pipes;
+using System.Security.Cryptography;
+
+namespace IPA.Cores.Tools.DebugHost
+{
+    public static class Utils
+    {
+        public static Process ExecProcess(string cmdLine)
+        {
+            DivideCommandLine(cmdLine, out string programName, out string arguments);
+
+            ProcessStartInfo info = new ProcessStartInfo()
+            {
+                FileName = programName,
+                Arguments = arguments,
+                UseShellExecute = false,
+                CreateNoWindow = false,
+            };
+
+            Process p = Process.Start(info);
+
+            return p;
+        }
+
+        public static bool IsSplitChar(char c, string splitStr)
+        {
+            return (splitStr.IndexOf("" + c, StringComparison.OrdinalIgnoreCase) != -1);
+        }
+
+        public static bool GetKeyAndValue(string str, out string key, out string value, string splitStr = " ")
+        {
+            uint mode = 0;
+            string keystr = "", valuestr = "";
+
+            foreach (char c in str)
+            {
+                switch (mode)
+                {
+                    case 0:
+                        if (IsSplitChar(c, splitStr) == false)
+                        {
+                            mode = 1;
+                            keystr += c;
+                        }
+                        break;
+
+                    case 1:
+                        if (IsSplitChar(c, splitStr) == false)
+                        {
+                            keystr += c;
+                        }
+                        else
+                        {
+                            mode = 2;
+                        }
+                        break;
+
+                    case 2:
+                        if (IsSplitChar(c, splitStr) == false)
+                        {
+                            mode = 3;
+                            valuestr += c;
+                        }
+                        break;
+
+                    case 3:
+                        valuestr += c;
+                        break;
+                }
+            }
+
+            if (mode == 0)
+            {
+                value = "";
+                key = "";
+                return false;
+            }
+            else
+            {
+                value = valuestr.Trim();
+                key = keystr.Trim();
+                return true;
+            }
+        }
+
+        public static string ByteToHex(byte[] data, string paddingStr)
+        {
+            StringBuilder ret = new StringBuilder();
+
+            int i;
+            for (i = 0; i < data.Length; i++)
+            {
+                byte b = data[i];
+
+                string s = b.ToString("X");
+                if (s.Length == 1)
+                {
+                    s = "0" + s;
+                }
+
+                ret.Append(s);
+
+                if (paddingStr != null)
+                {
+                    if (i != (data.Length - 1))
+                    {
+                        ret.Append(paddingStr);
+                    }
+                }
+            }
+
+            return ret.ToString().Trim();
+        }
+
+        public static byte[] HashSHA1(byte[] src)
+        {
+            SHA1 sha = new SHA1Managed();
+
+            return sha.ComputeHash(src);
+        }
+
+        public static void DivideCommandLine(string src, out string myProgramName, out string arguments)
+        {
+            int len = src.Length;
+            int mode = 0;
+            StringBuilder sb1 = new StringBuilder();
+            StringBuilder sb2 = new StringBuilder();
+
+            for (int i = 0; i < len; i++)
+            {
+                char c = src[i];
+                if (mode == 0)
+                {
+                    if (c == '\"')
+                    {
+                        mode = 1;
+                    }
+                    else if (c == ' ')
+                    {
+                        mode = 2;
+                    }
+                }
+                else if (mode == 1)
+                {
+                    if (c == '\"')
+                    {
+                        mode = 0;
+                    }
+                }
+                else if (mode == 2)
+                {
+                    if (c != ' ')
+                    {
+                        mode = 3;
+                    }
+                }
+
+                if (mode == 0 || mode == 1)
+                {
+                    sb1.Append(c);
+                }
+                else if (mode == 3)
+                {
+                    sb2.Append(c);
+                }
+            }
+
+            myProgramName = sb1.ToString();
+            arguments = sb2.ToString();
+        }
+    }
+
+    public struct Once
+    {
+        volatile private int flag;
+        public void Set() => IsFirstCall();
+        public bool IsFirstCall() => (Interlocked.CompareExchange(ref this.flag, 1, 0) == 0);
+        public bool IsSet => (this.flag != 0);
+        public static implicit operator bool(Once once) => once.flag != 0;
+        public void Reset() => this.flag = 0;
+
+        public override string ToString() => IsSet.ToString();
+    }
+
+    public class GlobalMutex
+    {
+        readonly string InternalName;
+
+        int LockedCount = 0;
+
+        Mutex CurrentMutexObj = null;
+
+        readonly static HashSet<object> MutexList = new HashSet<object>();
+
+        public GlobalMutex(string name)
+        {
+            InternalName = @"Global\utils_si_" + name;
+        }
+
+        public bool TryLock()
+        {
+            try
+            {
+                Lock();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void Lock()
+        {
+            if (LockedCount == 0)
+            {
+                Mutex mutex = new Mutex(false, InternalName, out bool createdNew);
+
+                if (createdNew == false)
+                {
+                    mutex.Dispose();
+                    throw new ApplicationException($"Cannot create the new mutex object.");
+                }
+
+                CurrentMutexObj = mutex;
+            }
+
+            LockedCount++;
+        }
+
+        public void Unlock()
+        {
+            if (LockedCount <= 0) throw new ApplicationException("locked_count <= 0");
+            if (LockedCount == 1)
+            {
+                CurrentMutexObj.Dispose();
+                CurrentMutexObj = null;
+            }
+            LockedCount--;
+        }
+    }
+}
