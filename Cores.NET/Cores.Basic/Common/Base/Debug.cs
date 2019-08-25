f57633039d07a740836bafcc545a953a2b0c980c
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2019-08-25 12:53:06 +0900

    バグ修正 Hello 26

--- Cores.NET/Cores.Basic/Common/Base/Debug.cs
+++ Cores.NET/Cores.Basic/Common/Base/Debug.cs
@@ -30,6 +30,8 @@
 // PROCESS MAY BE SERVED ON EITHER PARTY IN THE MANNER AUTHORIZED BY APPLICABLE
 // LAW OR COURT RULE.
 
+#pragma warning disable CA2235 // Mark all non-serializable fields
+
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -1617,6 +1619,7 @@ namespace IPA.Cores.Basic
         }
     }
 
+    [Serializable]
     public class DbgGitCommitInfo
     {
         public string CommitId { get; }
