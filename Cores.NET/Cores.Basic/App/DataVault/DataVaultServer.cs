b475cc35911cded7f5f582796c59520cb6e7821b
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2020-08-16 01:00:28 +0900

    all にいたしました

--- Cores.NET/Cores.Basic/App/DataVault/DataVaultServer.cs
+++ Cores.NET/Cores.Basic/App/DataVault/DataVaultServer.cs
@@ -79,6 +79,10 @@ namespace IPA.Cores.Basic
 
         public void NormalizeReceivedData(string defaultSrcMachineName)
         {
+            if (this.KeyType._IsEmpty()) this.KeyType = "all";
+            if (this.KeyShortValue._IsEmpty()) this.KeyShortValue = "all";
+            if (this.KeyFullValue._IsEmpty()) this.KeyFullValue = "all";
+
             this.SystemName = WinParser.MakeSafeFileName(this.SystemName._NonNullTrim()).ToLower()._TruncStr(Consts.MaxLens.DataVaultPathElementMaxLen);
             this.LogName = WinParser.MakeSafeFileName(this.LogName._NonNullTrim()).ToLower()._TruncStr(Consts.MaxLens.DataVaultPathElementMaxLen);
             this.KeyType = WinParser.MakeSafeFileName(this.KeyType._NonNullTrim()).ToLower()._TruncStr(Consts.MaxLens.DataVaultPathElementMaxLen);
