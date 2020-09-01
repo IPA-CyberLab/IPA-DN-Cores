bccc57bb898c905ff36d20adf5fece58fbe7585a
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2020-09-02 06:57:13 +0900

    Kestrel native stack

--- Cores.NET/Cores.Basic/App/SnmpWork/SnmpWork.cs
+++ Cores.NET/Cores.Basic/App/SnmpWork/SnmpWork.cs
@@ -353,7 +353,7 @@ namespace IPA.Cores.Basic
                     //LocalHostOnly = true,
                     HttpPortsList = new int[] { port }.ToList(),
                     HttpsPortsList = new List<int>(),
-                    UseKestrelWithIPACoreStack = true,
+                    UseKestrelWithIPACoreStack = false,
                 },
                 true);
             }
