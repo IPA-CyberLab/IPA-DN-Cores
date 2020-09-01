2103308b6b2431931caf30e8ecda0265de6b8fa7
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2020-09-02 04:39:24 +0900

    Kestrel 自前 Stack 利用

--- Cores.NET/Cores.Basic/App/SnmpWork/SnmpWork.cs
+++ Cores.NET/Cores.Basic/App/SnmpWork/SnmpWork.cs
@@ -353,7 +353,6 @@ namespace IPA.Cores.Basic
                     LocalHostOnly = true,
                     HttpPortsList = new int[] { port }.ToList(),
                     HttpsPortsList = new List<int>(),
-                    UseKestrelWithIPACoreStack = false,
                 },
                 true);
             }
