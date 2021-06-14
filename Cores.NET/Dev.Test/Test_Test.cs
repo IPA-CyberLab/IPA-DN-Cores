de8d3fc14969e0eb129fa4900f15b1966ba6ff7d
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-06-14 15:08:38 +0900

    aaa

--- Cores.NET/Dev.Test/Test_Test.cs
+++ Cores.NET/Dev.Test/Test_Test.cs
@@ -1822,7 +1822,7 @@ namespace IPA.TestDev
                                         }
                                         else
                                         {
-                                            if (true)
+                                            if (false)
                                             {
                                                 var res = await datagramBulkReceiver.RecvAsync(c, s);
                                                 measure.Add(res!.Length);
@@ -1832,6 +1832,7 @@ namespace IPA.TestDev
                                             else if (true)
                                             {
                                                 var result = await s.ReceiveFromAsync(mem);
+                                                mem.AsMemory()._CloneMemory();
 
                                                 //// 打ち返し
                                                 //await s.SendToAsync(mem, result.RemoteEndPoint);
