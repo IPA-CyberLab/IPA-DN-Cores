0bc02f7e2cbbd5e7f529c07260a4fd653d7d00af
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-07-12 10:56:51 +0900

    途中さん

--- Cores.NET/Cores.Codes/Codes/ThinController/ThinWebClient.cs
+++ Cores.NET/Cores.Codes/Codes/ThinController/ThinWebClient.cs
@@ -730,8 +730,8 @@ namespace IPA.Cores.Codes
                                     Client.SettingsFastSnapshot.Debug_GuacdMode_GuacdHostname,
                                     Client.SettingsFastSnapshot.Debug_GuacdPort,
                                     ready.FirstConnection!.SvcType.ToString().StrToGuaProtocol(),
-                                    "", ready.ListenEndPoint!.Port,
-                                    //"dn-ttwin1.sec.softether.co.jp", 3389, // testtest
+                                    //"", ready.ListenEndPoint!.Port,
+                                    "pc37.sec.softether.co.jp", 3333, // testtest
                                     pref));
 
                             var readyPacket = await guaClient.StartAsync(cancel);
