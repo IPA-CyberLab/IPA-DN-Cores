98345fcdec834d1147734299fcddeba83b8fc2f3
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-08-29 11:53:12 +0900

    test

--- Cores.NET/Dev.Test/Test_Test.cs
+++ Cores.NET/Dev.Test/Test_Test.cs
@@ -2300,7 +2300,7 @@ namespace IPA.TestDev
 
         public static void Test_Generic()
         {
-            if (false)
+            if (true)
             {
                 Test_210615_Udp_Indirect_SendRecv_Bench_DNS_Server_MultiTaskProcess();
                 return;
