14b7707b79072233855458a664e0dd98168dea29
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-10-12 09:04:45 +0900

    Slack bug fix (Decrease limit)

--- Cores.NET/Cores.Basic/Json/WebApi/ClientApi/SlackApi.cs
+++ Cores.NET/Cores.Basic/Json/WebApi/ClientApi/SlackApi.cs
@@ -113,7 +113,7 @@ namespace IPA.Cores.ClientApi.SlackApi
 
     public static class SlackApiSettings
     {
-        public static readonly Copenhagen<int> SlackApiLimitCounts = 33;
+        public static readonly Copenhagen<int> SlackApiLimitCounts = 50;
         public static readonly Copenhagen<int> SlackApiLimitCounts_Users = 1000;
     }
 
