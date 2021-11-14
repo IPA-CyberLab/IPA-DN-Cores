d3d9970bbd849cbd98a16b937b56cbc281a70f7c
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-11-14 17:18:56 +0900

    add symbolic link for compatibility

--- Cores.NET/Cores.Apps/SnmpWork/_SnmpWork_ShellScript.sh
+++ Cores.NET/Cores.Apps/SnmpWork/_SnmpWork_ShellScript.sh
@@ -11,6 +11,10 @@ export MALLOC_TOP_PAD_=131072
 export MALLOC_MMAP_MAX_=65536
 export HOME=/root
 
+mkdir -p /etc/se_snmpwork/Local/App_TestDev/Config/
+ln -s /etc/se_snmpwork/Local/App_TestDev/ /etc/se_snmpwork/Local/App_SnmpWork
+
+
 echo test > /etc/se_snmpwork/approot
 
 /etc/se_snmpwork/tmp/snmpwork/se_snmpwork2 $@
