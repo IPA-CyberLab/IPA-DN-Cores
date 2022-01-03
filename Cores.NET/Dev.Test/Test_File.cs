e57d15c2885361d05510dec7867e254bd46b1b03
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-01-03 20:17:35 +0900

    blocksize

--- Cores.NET/Dev.Test/Test_File.cs
+++ Cores.NET/Dev.Test/Test_File.cs
@@ -71,7 +71,7 @@ partial class TestDevCommands
 
         Async(async () =>
         {
-            int blockSize = 10 * 1000 * 1000;
+            int blockSize = 1000 * 1000;
             int randSeedSize = blockSize * 16;
 
             if (targetSize <= 0)
