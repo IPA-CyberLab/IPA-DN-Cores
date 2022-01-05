520f051aa74cc23a87c138bf00f14a119cacb753
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-01-06 04:36:00 +0900

    途中さん

--- Cores.NET/Cores.Basic/Database/Lib/Misc/HadbSqlScripts/2_Create_Users.sql
+++ Cores.NET/Cores.Basic/Database/Lib/Misc/HadbSqlScripts/2_Create_Users.sql
@@ -2,6 +2,15 @@
 
 USE [master]
 GO
+
+IF EXISTS 
+    (SELECT name  
+     FROM master.sys.server_principals
+     WHERE name = 'sql_hadb001_reader')
+BEGIN
+    DROP LOGIN [sql_hadb001_reader]
+END
+
 CREATE LOGIN [sql_hadb001_reader] WITH PASSWORD=N'sql_hadb_reader_default_password', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
 GO
 USE [HADB001]
@@ -20,6 +29,18 @@ GO
 
 USE [master]
 GO
+
+
+
+IF EXISTS 
+    (SELECT name  
+     FROM master.sys.server_principals
+     WHERE name = 'sql_hadb001_writer')
+BEGIN
+    DROP LOGIN [sql_hadb001_writer]
+END
+
+
 CREATE LOGIN [sql_hadb001_writer] WITH PASSWORD=N'sql_hadb_writer_default_password', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
 GO
 USE [HADB001]
