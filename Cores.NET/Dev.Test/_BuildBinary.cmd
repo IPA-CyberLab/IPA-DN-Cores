d990a0fa5150cfc7db2fa4d613edc9830cfd5a69
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-06-24 01:52:04 +0900

    途中さん

--- Cores.NET/Dev.Test/_BuildBinary.cmd
+++ Cores.NET/Dev.Test/_BuildBinary.cmd
@@ -1,6 +1,11 @@
 rmdir /s /q bld\
 
+rmdir /s /q c:\tmp\cores_built_binary\
+mkdir c:\tmp\cores_built_binary\
+
 mkdir bld\
+
+..\Dev.Tools\CompiledBin\WriteTimeStamp.exe > c:\tmp\cores_built_binary\TimeStamp.txt
 
 dotnet clean -c Release
 
@@ -10,16 +15,11 @@ dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true -p:PublishReady
 
 dotnet publish -c Release -r linux-arm64 -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:IncludeNativeLibrariesForSelfExtract=true --self-contained -o bld\linux-arm64\Dev.Test
 
-rmdir /s /q c:\tmp\cores_built_binary\
-mkdir c:\tmp\cores_built_binary\
-
 copy /y bld\win-x64\Dev.Test\Dev.Test.exe c:\tmp\cores_built_binary\Dev.Test.Win.x86_64.exe
 copy /y bld\linux-x64\Dev.Test\Dev.Test c:\tmp\cores_built_binary\Dev.Test.Linux.x86_64
 copy /y bld\linux-arm64\Dev.Test\Dev.Test c:\tmp\cores_built_binary\Dev.Test.Linux.aarch64
 
 S:\CommomDev\SE-DNP-CodeSignClientApp\SE-DNP-CodeSignClientApp_signed.exe SignDir c:\tmp\cores_built_binary\ /CERT:SoftEtherEv /COMMENT:'Dev.Test.Win.x86_64'
-
-..\Dev.Tools\CompiledBin\WriteTimeStamp.exe > c:\tmp\cores_built_binary\TimeStamp.txt
 
 cd /d c:\tmp\cores_built_binary\
 
