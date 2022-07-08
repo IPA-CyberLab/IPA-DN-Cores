a866047131a33be62ab2043bee1742467f606caf
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-07-08 10:08:17 +0900

    EmptyDaemon の追加

---
+++ Cores.NET/Cores.Basic/App/EmptyDaemonApp/EmptyDaemonApp.cs
@@ -0,0 +1,127 @@
+﻿// IPA Cores.NET
+// 
+// Copyright (c) 2019- IPA CyberLab.
+// Copyright (c) 2003-2018 Daiyuu Nobori.
+// Copyright (c) 2013-2018 SoftEther VPN Project, University of Tsukuba, Japan.
+// All Rights Reserved.
+// 
+// License: The Apache License, Version 2.0
+// https://www.apache.org/licenses/LICENSE-2.0
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// 
+// THIS SOFTWARE IS DEVELOPED IN JAPAN, AND DISTRIBUTED FROM JAPAN, UNDER
+// JAPANESE LAWS. YOU MUST AGREE IN ADVANCE TO USE, COPY, MODIFY, MERGE, PUBLISH,
+// DISTRIBUTE, SUBLICENSE, AND/OR SELL COPIES OF THIS SOFTWARE, THAT ANY
+// JURIDICAL DISPUTES WHICH ARE CONCERNED TO THIS SOFTWARE OR ITS CONTENTS,
+// AGAINST US (IPA CYBERLAB, DAIYUU NOBORI, SOFTETHER VPN PROJECT OR OTHER
+// SUPPLIERS), OR ANY JURIDICAL DISPUTES AGAINST US WHICH ARE CAUSED BY ANY KIND
+// OF USING, COPYING, MODIFYING, MERGING, PUBLISHING, DISTRIBUTING, SUBLICENSING,
+// AND/OR SELLING COPIES OF THIS SOFTWARE SHALL BE REGARDED AS BE CONSTRUED AND
+// CONTROLLED BY JAPANESE LAWS, AND YOU MUST FURTHER CONSENT TO EXCLUSIVE
+// JURISDICTION AND VENUE IN THE COURTS SITTING IN TOKYO, JAPAN. YOU MUST WAIVE
+// ALL DEFENSES OF LACK OF PERSONAL JURISDICTION AND FORUM NON CONVENIENS.
+// PROCESS MAY BE SERVED ON EITHER PARTY IN THE MANNER AUTHORIZED BY APPLICABLE
+// LAW OR COURT RULE.
+
+// Author: Daiyuu Nobori
+// Description
+
+#if CORES_BASIC_JSON && (CORES_BASIC_WEBAPP || CORES_BASIC_HTTPSERVER) && CORES_BASIC_SECURITY
+
+#pragma warning disable CA2235 // Mark all non-serializable fields
+
+using System;
+using System.Buffers;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+
+using IPA.Cores.Basic;
+using IPA.Cores.Helper.Basic;
+using static IPA.Cores.Globals.Basic;
+
+namespace IPA.Cores.Basic;
+
+public static partial class CoresConfig
+{
+    public static partial class EmptyDaemonHost
+    {
+        public static readonly Copenhagen<string> _Test = "Hello";
+    }
+}
+
+public class EmptyDaemonSettings : INormalizable
+{
+    public string _TestStr = "";
+
+    public void Normalize()
+    {
+        if (this._TestStr._IsEmpty())
+        {
+            this._TestStr = "Hello";
+        }
+    }
+}
+
+
+public class EmptyDaemonApp : AsyncService
+{
+    readonly HiveData<EmptyDaemonSettings> SettingsHive;
+
+    // 'Config\EmptyDaemon' のデータ
+    public EmptyDaemonSettings Settings => SettingsHive.GetManagedDataSnapshot();
+
+    readonly CriticalSection LockList = new CriticalSection<EmptyDaemonApp>();
+
+    public EmptyDaemonApp()
+    {
+        try
+        {
+            // Settings を読み込む
+            this.SettingsHive = new HiveData<EmptyDaemonSettings>(Hive.SharedLocalConfigHive, $"EmptyDaemon", null, HiveSyncPolicy.AutoReadFromFile);
+
+
+            // TODO: ここでサーバーを立ち上げるなどの初期化処理を行なう
+        }
+        catch
+        {
+            this._DisposeSafe();
+            throw;
+        }
+    }
+
+
+    protected override async Task CleanupImplAsync(Exception? ex)
+    {
+        try
+        {
+            // TODO: ここでサーバーを終了するなどのクリーンアップ処理を行なう
+
+            this.SettingsHive._DisposeSafe();
+        }
+        finally
+        {
+            await base.CleanupImplAsync(ex);
+        }
+    }
+}
+
+#endif
+
